ae8646d6f215331d8a937be3ad102ff7310323ab
Rasel Parvez Sanny <raselparvezsanny@gmail.com>
2024-11-24 13:19:02 +0600

    create: home layout created

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,11 +1,17 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Main from '../layout/Main';
+import Home from '../pages/Home';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Main />,
-    children: [{}],
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+    ],
   },
 ]);
 export default router;

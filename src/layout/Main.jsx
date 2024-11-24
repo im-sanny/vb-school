9fdcedd2eb5b1f0a4794680eca29f310328db2aa
Rasel Parvez Sanny <raselparvezsanny@gmail.com>
2024-11-24 11:56:41 +0600

    add: added router and router layout

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,13 +1,14 @@
-import React from "react";
+import { Outlet } from 'react-router-dom';
 
 const Main = () => {
   return (
     <div>
       {/* Navbar */}
-
+      <h1 className="text-5xl text-red-500">Navbar</h1>
       {/* Outlet */}
-
+      <Outlet />
       {/* Footer */}
+      <h1>footer</h1>
     </div>
   );
 };
